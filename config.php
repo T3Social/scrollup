feaf7ffe5d32dce4877c3d898a96bfab8f779f6f
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-04-09 20:12:15 -0400

    Enh: Add scroll up to Spaces

--- config.php
+++ config.php
@@ -5,14 +5,16 @@ namespace humhub\modules\scrollup;
 use humhub\modules\scrollup\Events;
 use humhub\modules\scrollup\Module;
 use humhub\modules\admin\widgets\AdminMenu;
-use humhub\modules\dashboard\widgets\Sidebar;
+use humhub\modules\space\widgets\Sidebar as Space;
+use humhub\modules\dashboard\widgets\Sidebar as Dashboard;
 
 return [
     'id' => 'scrollup',
     'class' => Module::class,
     'namespace' => 'humhub\modules\scrollup',
     'events' => [
-        ['class' => Sidebar::class, 'event' => Sidebar::EVENT_INIT, 'callback' => [Events::class, 'onScrollUp']],
+        ['class' => Dashboard::class, 'event' => Dashboard::EVENT_INIT, 'callback' => [Events::class, 'onScrollUp']],
+        ['class' => Space::class, 'event' => Space::EVENT_INIT, 'callback' => [Events::class, 'onScrollUp']],
     ]
 ];
 ?>
