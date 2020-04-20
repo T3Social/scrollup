10d158e868872e99f4f567b36a32928429a9f42f
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-04-20 17:10:52 -0400

    Enh: Added getConfigUrl()

--- Module.php
+++ Module.php
@@ -2,10 +2,19 @@
 
 namespace humhub\modules\scrollup;
 
+use yii\helpers\Url;
 use humhub\components\Module as BaseModule;
 
 class Module extends BaseModule
 {
+
+    /**
+     * @inheritdoc
+     */
+    public function getConfigUrl()
+    {
+        return Url::to(['/scrollup/admin/index']);
+    }
 
     public function getPosition()
     {
