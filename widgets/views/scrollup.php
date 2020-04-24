53bb85b2ac77d80efae26c012db0235bc361bbeb
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-04-23 20:55:09 -0400

    Update scrollup.php

--- widgets/views/scrollup.php
+++ widgets/views/scrollup.php
@@ -3,6 +3,11 @@
 use yii\helpers\Url;
 use humhub\libs\Html;
 use humhub\models\Setting;
+use humhub\modules\ui\view\components\View;
+
+/* @var $position string */
+/* @var $color string */
+/* @var $this View */
 
 $this->registerJs("var btn = $('#button');
 
