947ae559db53986e5774791febbf2a560edf0662
buddh4 <mail@jharrer.de>
2020-04-24 13:57:49 +0200

    - Fix: ConfigureForm not validated - Enh: Added color picker - Chg: Advanced input validation - Chg: Removed admin menu item - Enh: Added translatable text - Enh: Auto position calculation - Enh: Use @default theme variable as default color - Enh: Refactored js and views - Fix: Removed pjax workaround

--- Events.php
+++ Events.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace humhub\modules\scrollup;
+namespace gm\humhub\modules\scrollup;
 
 use Yii;
 use yii\helpers\Url;
@@ -9,40 +9,14 @@ use humhub\models\Setting;
 
 class Events extends BaseObject
 {
-
      /**
      * @param \yii\base\Event $event
      * @throws \Exception
      * @throws \Throwable
      */
-    public static function onAdminMenuInit($event)
+    public static function onLayoutAddonsInit($event)
     {
-        $event->sender->addItem([
-            'label' => 'Scroll Up Settings',
-            'url' => Url::toRoute('/scrollup/admin/index'),
-            'group' => 'settings',
-            'icon' => '<i class="fa fa-chevron-up"></i>',
-            'isActive' => Yii::$app->controller->module && Yii::$app->controller->module->id == 'scrollup' && Yii::$app->controller->id == 'admin',
-            'sortOrder' => 650
-        ]);
-    }
-
-     /**
-     * @param \yii\base\Event $event
-     * @throws \Exception
-     * @throws \Throwable
-     */
-    public static function onScrollUp($event)
-    {
-        // Do not allow guest view force logged in
-        //if (Yii::$app->user->isGuest) {
-        //    return \humhub\modules\user\models\User::EVENT_CHECK_VISIBILITY;
-        //}
-
-        $event->sender->view->registerAssetBundle(Assets::class);
-        $event->sender->addWidget(widgets\ScrollUp::class, [], [
-            'sortOrder' => Setting::Get('timeout', 'scrollup')
-            ]);
+        $event->sender->addWidget(widgets\ScrollUp::class);
 
     }
 }
