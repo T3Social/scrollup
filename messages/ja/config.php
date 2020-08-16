ccdc60c559a950f6d9f5f1867532620ad9670645
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-08-16 15:50:52 -0400

    Enh: Japanese translations

--- messages/ja/config.php
+++ messages/ja/config.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    '<strong>Scroll Up</strong> module configuration' => '',
-    'Color:' => '',
-    'Invalid color!' => '',
-    'Position:' => '',
-    'Save' => '',
-    'Select color ...' => '',
-    'e.g. <code>#000000</code> (default: your themes @default color variable).' => '',
-    'e.g: <code>left: 30px;</code> or <code>right: 30px;</code> (default: calculated center of stream sidebar)' => '',
+    '<strong>Scroll Up</strong> module configuration' => '<strong>上にスクロール</strong>モジュールの構成',
+    'Color:' => '色:',
+    'Invalid color!' => '無効な色です！',
+    'Position:' => 'ポジション:',
+    'Save' => '保存する',
+    'Select color ...' => '色を選択してください...',
+    'e.g. <code>#000000</code> (default: your themes @default color variable).' => '例えば<code>#000000</code>（デフォルト：テーマの@defaultカラー変数）。',
+    'e.g: <code>left: 30px;</code> or <code>right: 30px;</code> (default: calculated center of stream sidebar)' => '例: <code>left: 30px;</code>または<code>right: 30px;</code>（デフォルト：計算されたストリームサイドバーの位置）',
 ];
