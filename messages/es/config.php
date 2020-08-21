dbc405f13ec4245eb2f08c90877d4e3b6396517d
xdCL <67495563+xdCL@users.noreply.github.com>
2020-08-20 23:19:45 -0400

    Update ES translation for Scroll Up module!
    It has been very useful for me, everything that ArchBlood and GreenMeteor have delivered in my experience in humhub, without you it would not be possible to carry out my educational project. so I will be supporting the translation of the modules into Spanish since it is my native language.
    
    Best regards.

--- messages/es/config.php
+++ messages/es/config.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    '<strong>Scroll Up</strong> module configuration' => '',
-    'Color:' => '',
-    'Invalid color!' => '',
-    'Position:' => '',
-    'Save' => '',
-    'Select color ...' => '',
-    'e.g. <code>#000000</code> (default: your themes @default color variable).' => '',
-    'e.g: <code>left: 30px;</code> or <code>right: 30px;</code> (default: calculated center of stream sidebar)' => '',
+    '<strong>Scroll Up</strong> module configuration' => 'Ajustes del módulo <strong>Scroll up</strong>',
+    'Color:' => 'Color:',
+    'Invalid color!' => 'Color inválido!',
+    'Position:' => 'Posición:',
+    'Save' => 'Guardar',
+    'Select color ...' => 'Elegir color ...',
+    'e.g. <code>#000000</code> (default: your themes @default color variable).' => 'por ejemplo: <code>#000000</code> (por defecto: variable de color @default de tu tema).',
+    'e.g: <code>left: 30px;</code> or <code>right: 30px;</code> (default: calculated center of stream sidebar)' => 'por ejemplo: <code>left: 30px;</code> o <code>right: 30px;</code>',
 ];
